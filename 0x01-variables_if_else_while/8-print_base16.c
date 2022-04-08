e151b8accbff860e8fd075be0c92fc5e6cb1b268
Franklin Obasi <franklinobasy@gmail.com>
2022-04-08 14:43:35 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char ch;
+
+	i = 0;
+	ch = 97;
+	while (i <= 16)
+	{
+		if (i > 9)
+		{
+			putchar(ch);
+			ch++;
+			i++;
+		}
+		else
+		{
+			putchar(i + '0');
+			i++;
+		}
+		putchar(10);
+		return (0);
+	}
+}
