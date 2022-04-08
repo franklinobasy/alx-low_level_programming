87cadb7f6af71b70fa49b1376a7e60c63e16f43a
Franklin Obasi <franklinobasy@gmail.com>
2022-04-08 16:06:40 -0700

    combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0, i < 9, i++)
+	{
+		for (j = 1, j < 10, j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 && j != 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
