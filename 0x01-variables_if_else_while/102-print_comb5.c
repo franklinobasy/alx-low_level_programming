8055c2aeb34e6d02917e22badb60f3b1610020cd
Franklin Obasi <franklinobasy@gmail.com>
2022-04-09 12:51:57 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,18 +22,18 @@ int main(void)
 			/* second numbers group*/
 			c = j / 10;
 			d = j % 10;
-			if ( j > i)
+			if ( i < j)
 			{
 				putchar(a + '0');
 				putchar(b + '0');
 				putchar(32); /* space number group*/
 				putchar(c + '0');
 				putchar(d + '0');
-				if (i != 98 && j != 99)
-				{
-					putchar(44);
-					putchar(32);
-				}
+			}
+			if ((i < j) && (i != 98) && (j != 99))
+			{
+				putchar(44);
+				putchar(32);
 			}
 		}
 	}
