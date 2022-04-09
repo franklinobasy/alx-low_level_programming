d459da22b12ac05c0e1d3f6dbcd54f2eed7dd306
Franklin Obasi <franklinobasy@gmail.com>
2022-04-08 17:10:36 -0700

    software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int w;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (w = 1; w < 10; w++)
+				{
+					if (j < k)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(32);
+						putchar(k + '0');
+						putchar(w + '0');
+						if (i == 9 && j == 8 && k == 9 && w == 9)
+							putchar(10);
+						else
+						{
+							putchar(44);
+							putchar(32);
+						}
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
