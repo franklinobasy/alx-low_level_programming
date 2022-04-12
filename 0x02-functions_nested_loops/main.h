017c0a2e76fccaccee0935c506a066d97e0ce9fd
Franklin Obasi <franklinobasy@gmail.com>
2022-04-12 09:09:16 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,20 @@
 #include <stdio.h>
+/**
+ * print_alphabet - To print all alphabets in lower case
+ *
+ * Return: void null
+ */
 int main(void);
+void print_alphabet(void);
+void print_alphabet(void)
+{
+	int c;
+
+	c = 97;
+	for (i = 1; i <= 26; i++)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+}
