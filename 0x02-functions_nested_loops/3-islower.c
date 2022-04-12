8efb43af320d480f67c50805210ea51534535f51
Franklin Obasi <franklinobasy@gmail.com>
2022-04-12 12:49:28 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "headers.h"
+
+/**
+ * _islower - check if a character is lower caser
+ *
+ * Return: throws 1 else throws 0
+ */
+int _islower(int c)
+{
+	if ((c >= 97) && (c <= 122))
+		return (1);
+	else
+		return (0);
+}
