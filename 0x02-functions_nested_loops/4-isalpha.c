e43f5d145ee1ac7f146362f462ecebb6dfa480f3
Franklin Obasi <franklinobasy@gmail.com>
2022-04-12 13:01:54 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+/**
+ * _isalpha - check if a letter is lowercase and uppercase
+ * @c : letter to be checked
+ *
+ * Return : throws 1 if true else 0
+ */
+int _isalpha(int c)
+{
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+		return (1);
+	else
+		return (0);
+}
