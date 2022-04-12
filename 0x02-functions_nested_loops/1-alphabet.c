b6568f6102e0737ff036d985c61809018ee18a86
Franklin Obasi <franklinobasy@gmail.com>
2022-04-12 11:44:07 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,17 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet - prints alphabet in lowercase, followed by a new line
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	int c;
+        int i;
+
+	c = 97;
+	for (i = 1; i <= 26, i++)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar(10);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,27 +1,14 @@
-#include <stdio.h>
 #ifndef MAIN_H_
 #define MAIN_H_
 
+/*
+ * File: main.h
+ * Auth: Franklin Obasi
+ * Desc: Header file containing declaration for all functions used
+ * 	 in the 0x02-functions_nested_loops directory
+ */
+int main(void);
 void print_alphabet(void);
 
 #endif
 
-/**
- * print_alphabet - To print all alphabets in lower case
- *
- * Return: void null
- */
-
-void print_alphabet(void)
-{
-	int c;
-	int i;
-
-	c = 97;
-	for (i = 1; i <= 26; i++)
-	{
-		putchar(c);
-		c++;
-	}
-	putchar(10);
-}
