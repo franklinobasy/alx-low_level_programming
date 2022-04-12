d7ff5471ba0ae7832aa2b9cbe87205c391ddccee
Franklin Obasi <franklinobasy@gmail.com>
2022-04-12 12:52:13 -0700

    task 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@
  * Desc: Header file containing declaration for all functions used
  * 	 in the 0x02-functions_nested_loops directory
  */
+int _putchar(char);
 int main(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
