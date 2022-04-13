e54823b00f2ecdb22e4869111c61c7c399d9492c
Franklin Obasi <franklinobasy@gmail.com>
2022-04-13 13:32:49 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desc: program that prints the first 50 Fibonacci
+ * numbers, starting with 1 and 2
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, prev, next, ans;
+
+	prev = 1;
+	next = 1;
+	ans = prev + next;
+	printf("%d, %d, ", next, ans);
+	for (i = 1; i <= 48; i++)
+	{
+		prev = next;
+		next = ans;
+		ans = prev + next;
+		if (i == 48)
+			printf("%d\n", ans);
+		else
+			printf("%d, ", ans);
+	}
+	return (0);
+}
+
+
