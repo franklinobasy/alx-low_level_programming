4268902f38cb7348358a9dd002883ad8654b911b
Franklin Obasi <franklinobasy@gmail.com>
2022-04-13 09:34:23 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_times_table - output times table
+ * @n - specified number
+ */
+void print_times_table(int n)
+{
+	int product;
+	int i;
+	int j;
+
+	if ((n >= 0) && (n <= 15))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(48);
+			for (j = 1; j <= n; j++)
+			{
+				product = i * j;
+				putchar(',');
+				putchar(' ');
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else
+				{
+					_putchar(product / 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+			}
+			putchar('\n');
+		}
+	}
+}
+
