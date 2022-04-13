0fcb2b753fe843a7a122d3988d0fa5e6b6f8e0d1
Franklin Obasi <franklinobasy@gmail.com>
2022-04-13 15:39:58 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,22 +10,22 @@
  */
 int main(void)
 {
-	unsigned long long prev, next, ans;
+	unsigned long prev, next, ans;
 	int i;
 
 	i = 1;
 	prev = 1;
 	next = 2;
-	printf("%llu, %llu, ", prev, next);
+	printf("%lu, %lu, ", prev, next);
 	while (i <= 97)
 	{
 		ans = prev + next;
 		prev = next;
 		next = ans;
 		if (i == 97)
-			printf("%llu\n", ans);
+			printf("%lu\n", ans);
 		else
-			printf("%llu, ", ans);
+			printf("%lu, ", ans);
 		i++;
 	}
 	return (0);
