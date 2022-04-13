015527b07725ee2d5b26c2bfe6b7cd794debc1ca
Franklin Obasi <franklinobasy@gmail.com>
2022-04-13 08:50:00 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print ordered number from n to 98
+ * @n: number to use
+ */
+void print_to_98(int n)
+{
+	int val;
+
+	val = n;
+	printf("%d, ", val);
+	while (val <= 98)
+	{
+		if (val == 98)
+			printf("%d", val);
+		else
+			printf("%d, ", val);
+		val++;
+	}
+}
