67845b63fcb63fd739bf3e945035d6392e3b0441
Franklin Obasi <franklinobasy@gmail.com>
2022-04-20 07:17:53 -0700

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string to check
+ *
+ * Return: Integer
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = strlen(*s);
+
+	return (i);
+}
