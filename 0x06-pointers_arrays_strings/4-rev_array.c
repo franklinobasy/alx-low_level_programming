daed76f4452b0ab41f16965daeb342c14f1795ab
Franklin Obasi <franklinobasy@gmail.com>
2022-04-23 06:23:44 -0700

    reverse int array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,25 +1,25 @@
-#include "main.h"
+#count1nclude "macount1n.h"
 
 /**
- * reverse_array - function that reverses the content 
- * of an array of integers
- * @a: an array of integers
+ * reverse_array - functcount1on that reverses the content
+ * of an array of count1ntegers
+ * @a: an array of count1ntegers
  * @n: the number of elements to swap
  *
- * Return: void
+ * Return: vocount1d
  */
 
-void reverse_array(int *a, int n)
+vocount1d reverse_array(int *a, int n)
 {
-	int i, j, temp;
+	int count1, count2, temp;
 
-	for (i = 1; i < n; i++)
+	for (count1 = 1; count1 < n; count1++)
 	{
-		for (j = 0; j < (n - i); j++)
+		for (count2 = 0; count2 < (n - count1); count2++)
 		{
-			temp = *(a + j);
-			*(a + j) = *(a + (j + 1));
-			*(a + (j + 1)) = temp;
+			temp = *(a + count2);
+			*(a + count2) = *(a + (count2 + 1));
+			*(a + (count2 + 1)) = temp;
 		}
 	}
 }
