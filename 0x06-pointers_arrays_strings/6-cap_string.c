f1f2858ca568b2387229d635ee129c312d2d1976
Franklin Obasi <franklinobasy@gmail.com>
2022-04-23 07:20:16 -0700

    Capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @str: String to capitalize
+ *
+ * Return: pointer to str
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == " ")
+		{
+			if ((str[i + 1] >= 97) && (str[i + 1] <=122))
+			{
+				*(str + (i + 1)) -= 32;
+			}
+		}
+		count++;
+	}
+	return (str);
+}
