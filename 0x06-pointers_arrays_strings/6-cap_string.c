ec46878c4e6795bd400fce776658192f181f6f1e
Franklin Obasi <franklinobasy@gmail.com>
2022-04-24 05:53:43 -0700

    String Capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@
 char *cap_string(char *str)
 {
 	int i, j, found;
-	char sep[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	i = 0;
 	found = 0;
@@ -30,4 +30,5 @@ char *cap_string(char *str)
 			*(str + (i + 1)) -= 32;
 		i++;
 	}
-}	
+	return (str);
+}
