5372876d0201e18db57447bba89a42ef6a0edb90
Franklin Obasi <franklinobasy@gmail.com>
2022-04-24 17:17:02 -0700

    ROT13 encrypt

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @str: String to encrypt
+ *
+ * Return: pointer to str
+ */
+
+char *rot13(char *str)
+{
+	int count1, count2;
+
+	count1 = 0;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count1) != '\0')
+	{
+		count2 = 0;
+		while (count2 < 52)
+		{
+			if (*(str + count1) == alphabet[count2])
+			{
+				*(s + count1) = rot13[count2];
+				break;
+			}
+			count2++;
+		}
+		count1++;
+	}
+	return (str);
+}
