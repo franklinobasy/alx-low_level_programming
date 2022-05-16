651d2de705a490189be7295851162a196cbbaac5
Obasi Franklin <58641266+franklinobasy@users.noreply.github.com>
2022-05-16 13:40:05 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,12 +19,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		if (s[0] == ops->op[i])
 			break;
-		i+=2;
+		i++;
 	}
 
-	return (ops[i].f);
+	return (ops[i / 2].f);
 }
