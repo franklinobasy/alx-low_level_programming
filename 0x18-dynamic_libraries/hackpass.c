fb5a0d9242fb549d6bd1c9fb1b87341fa9dc8b73
Franklin Obasi <franklinobasy@gmail.com>
2022-07-12 22:52:04 +0100

    3. Code injection: Win the Giga Millions

--- 0x18-dynamic_libraries/hackpass.c
+++ 0x18-dynamic_libraries/hackpass.c
@@ -2,16 +2,16 @@
 #include <unistd.h>
 
 /**
- * puts - pretends to be the c puts function
+ * srand - pretends to be the c srand function
  *
  * Returns: 9, 8, 10, 24, 75 and 9 sequentially
  */
-int puts(const char *s)
+void srand(unsigned int seed)
 {
 	const char *nums_txt = "9 8 10 24 75 - 9\n";
 	const char *txt = "Congratulations, you win the Jackpot!\n";
 
-	//(void)seed;
+	(void)seed;
 	write(STDOUT_FILENO, (void *)nums_txt, 17);
 	write(STDOUT_FILENO, (void *)txt, 38);
 	exit(EXIT_SUCCESS);
