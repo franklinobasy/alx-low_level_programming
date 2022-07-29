1ec76f9e7e151176f4f4d0df3f1d57f2726d9365
Franklin Obasi <franklinobasy@gmail.com>
2022-07-30 00:49:58 +0100

    djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,7 +1,10 @@
+#include "hash_tables.h"
+
 /**
- * hash_djb2 - hash function implementing the djb2 algorithm
- * @str: key
- * Return: hashed num
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
